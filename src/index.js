c9769ee8b64a84a96bc74b57faeffc1127537e9a
DonatoRM <donatoramos@hotmail.com>
2025-03-01 14:14:16 +0100

    nueva versión

--- src/index.js
+++ src/index.js
@@ -1,126 +1,104 @@
 /*
-Examen GRI
+Examen GRI primer módulo
 Autor: Donato Ramos Martínez
 */
 'use strict';
-const taskList = [];
-const form = document.getElementById('form');
-const task = document.getElementById('task');
-const error = document.getElementById('error');
-const response = document.getElementById('response');
-const accessibility = document.getElementById('accessibility');
-document.addEventListener(
-    'DOMContentLoaded',
-    () => {
-        form.addEventListener(
-            'submit',
-            (event) => {
-                event.preventDefault();
-                const taskValue = task.value.trim();
-                if (taskValue === '') {
-                    const spanError = document.createElement('span');
-                    spanError.classList.add('error');
-                    spanError.textContent = 'Campo vacío. Introduce una tarea';
-                    error.appendChild(spanError);
-                    const idError = setTimeout(
-                        () => (error.textContent = ''),
-                        1500
-                    );
-                    task.focus();
-                } else {
-                    taskList.push({
-                        task: taskValue,
-                        completed: false
-                    });
-                    task.value = '';
-                    task.focus();
-                    readTasks();
-                }
-            },
-            false
-        );
-        accessibility.addEventListener(
-            'click',
-            () => {
-                const body = document.querySelector('body');
-                if (body.classList.contains('accessibility')) {
-                    body.classList.remove('accessibility');
-                    document.querySelectorAll('button').forEach((button) => {
-                        button.classList.remove('accessibility');
-                    });
-                    task.classList.remove('accessibility');
-                } else {
-                    body.classList.add('accessibility');
-                    document.querySelectorAll('button').forEach((button) => {
-                        button.classList.add('accessibility');
-                    });
-                    task.classList.add('accessibility');
-                }
-            },
-            false
-        );
+const taskList = []; // Lista de tareas inicial
+const form = document.getElementById('form'); // Formulario de entrada de Tareas
+const inputTask = document.getElementById('task'); // Input de entrada de Tarea
+const divErrors = document.getElementById('error'); // Div en donde aparecerán los errores
+const ulResponse = document.getElementById('response'); // Lista en donde se insertarán las Tareas
+/**
+ *
+ * @param {String} value Texto que queremos que aparezca dentro del botón
+ * @param {Function} callback Función que se ejecutará cuando se haga click en el botón
+ * @returns Devuelve el botón debidamente creado
+ */
+const createButton = (value, callback) => {
+    const button = document.createElement('button');
+    button.textContent = value;
+    button.setAttribute('aria-pressed', 'false');
+    button.addEventListener('click', callback, false);
+    return button;
+};
+/**
+ * Función que cambia el estado del objeto que almacena la tarea seleccionada
+ * @param {*} event Evento que se dispara al hacer click en el botón
+ */
+const toggleCompleted = (event) => {
+    const button = event.target;
+    const li = event.target.parentElement;
+    const index = parseInt(li.getAttribute('data-index'));
+    taskList.forEach((taskItem, id) => {
+        if (id === index) {
+            taskItem.completed = !taskItem.completed;
+        }
+    });
+    showTaskList();
+};
+/**
+ * Función que borra la tarea seleccionada de la lista de tareas
+ * @param {*} event Evento que se produce cuando se hace clic sobre el botón de borrar
+ */
+const deleteTask = (event) => {
+    const index = Number(event.target.parentElement.getAttribute('data-index'));
+    taskList.splice(index, 1);
+    showTaskList();
+};
+/**
+ * Función que muestra la lista de tareas en el DOM
+ */
+const showTaskList = () => {
+    ulResponse.innerHTML = '';
+    if (taskList.length > 0) {
+        taskList.forEach((task, index) => {
+            const li = document.createElement('li');
+            li.textContent = task.task;
+            li.setAttribute('data-index', index);
+            li.setAttribute('role', 'list-item');
+            li.classList.toggle('completed', task.completed);
+            li.appendChild(
+                createButton(
+                    task.completed ? 'deshacer' : 'completar',
+                    toggleCompleted
+                )
+            );
+            li.appendChild(createButton('borrar', deleteTask));
+            ulResponse.appendChild(li);
+        });
+    }
+};
+/**
+ * Función que muestra un mensaje de error temporizado en el DOM
+ */
+const throwError = () => {
+    const TIME_ERROR = 1500; // 1.5 segundos
+    const spanError = document.createElement('span');
+    spanError.textContent = 'La tarea insertada no es válida';
+    spanError.classList = 'error';
+    divErrors.appendChild(spanError);
+    const idResume = setTimeout(() => {
+        divErrors.innerHTML = '';
+    }, TIME_ERROR);
+};
+// Se controla el evento submit del formulario para ejecutar la política de negocio
+form.addEventListener(
+    'submit',
+    (event) => {
+        event.preventDefault();
+        const taskValue = inputTask.value.trim();
+        // Comprobamos si dentro del input de tarea hay algo escrito
+        if (taskValue === '') {
+            throwError();
+        } else {
+            taskList.push({
+                task: taskValue,
+                completed: false
+            });
+            showTaskList();
+            inputTask.value = '';
+        }
+        inputTask.focus();
     },
     false
 );
-const readTasks = () => {
-    response.innerHTML = '';
-    taskList.forEach((task) => {
-        const li = document.createElement('li');
-        if (task.completed) {
-            li.classList.add('completed');
-        } else {
-            li.classList.remove('completed');
-        }
-        li.textContent = task.task;
-        const buttonCompleted = document.createElement('button');
-        if (task.completed) {
-            buttonCompleted.setAttribute('aria-pressed', 'true');
-            buttonCompleted.textContent = 'restablecer';
-        } else {
-            buttonCompleted.setAttribute('aria-pressed', 'false');
-            buttonCompleted.textContent = 'Completar';
-        }
-        buttonCompleted.setAttribute('aria-label', 'Botón de completar tarea');
-
-        const buttonDelete = document.createElement('button');
-        buttonDelete.setAttribute('aria-label', 'Botón de eliminar tarea');
-        buttonDelete.textContent = 'Eliminar';
-        buttonCompleted.addEventListener('click', taskCompleted, false);
-        buttonDelete.addEventListener('click', taskDelete, false);
-        li.appendChild(buttonCompleted);
-        li.appendChild(buttonDelete);
-        response.appendChild(li);
-    });
-};
-const taskCompleted = (event) => {
-    const button = event.target;
-    const li = button.parentElement;
-    console.log('Botón:', button); // Verifica el botón
-    if (button.getAttribute('aria-pressed') === 'false') {
-        button.setAttribute('aria-pressed', 'true');
-        taskList.forEach((task) => {
-            if (task.task === li.firstChild.textContent) {
-                task.completed = true;
-            }
-        });
-    } else {
-        button.setAttribute('aria-pressed', 'false');
-        taskList.forEach((task) => {
-            if (task.task === li.firstChild.textContent) {
-                task.completed = false;
-            }
-        });
-    }
-    readTasks();
-};
-const taskDelete = (event) => {
-    const button = event.target;
-    const li = button.parentElement;
-    const index = taskList.findIndex((element) => {
-        return element.task === li.firstChild.textContent;
-    });
-    if (index !== -1) {
-        taskList.splice(index, 1);
-    }
-    readTasks();
-};
